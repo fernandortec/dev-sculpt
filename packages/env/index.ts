71badf92e20a31980dd96c834b6232ffe6db4def
Fernando Rodrigues <fernandorfigueiredotec@gmail.com>
2024-05-08 12:37:07 -0300

    :wrench: (env) FEAT: add env configuration and basic linting

---
+++ packages/env/index.ts
@@ -0,0 +1,18 @@
+import { createEnv } from "@t3-oss/env-nextjs";
+import { z } from "zod";
+
+export const env = createEnv({
+	server: {
+		SERVER_PORT: z.coerce.number().default(3333),
+		DATABASE_URL: z.string().url(),
+		JWT_SECRET: z.string(),
+	},
+	client: {},
+	shared: {},
+	runtimeEnv: {
+		DATABASE_URL: process.env.DATABASE_URL,
+		JWT_SECRET: process.env.JWT_SECRET,
+		SERVER_PORT: process.env.SERVER_PORT,
+	},
+	emptyStringAsUndefined: true,
+});
