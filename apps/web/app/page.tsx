7ffd5a29fd3d5dc7f252563a279e049b5a37be0e
Fernando Rodrigues <fernandorfigueiredotec@gmail.com>
2024-05-09 17:09:04 -0300

    :sparkles: (home) Feat: add home page with hero form

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -1,7 +1,85 @@
-export default function Page(): JSX.Element {
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
+import { BlueStripes } from "@/public/assets/blue-stripes";
+import { OrangeStripes } from "@/public/assets/orange-stripes";
+import {
+	BriefcaseIcon,
+	FlowerIcon,
+	HeartIcon,
+	LightbulbIcon,
+	MountainSnow,
+	SearchIcon,
+	SettingsIcon,
+} from "lucide-react";
+
+export default function Page() {
 	return (
-		<div>
-			<p>opa</p>
-		</div>
+		<>
+			<main className="relative isolate h-screen overflow-hidden">
+				<div className="-z-10 absolute inset-0 bg-[radial-gradient(45rem_50rem_at_top,theme(colors.indigo.100),white)] opacity-20" />
+				<div className="-z-10 absolute inset-y-0 right-1/2 mr-16 w-[200%] origin-bottom-left skew-x-[-30deg] bg-white shadow-indigo-600/10 shadow-xl ring-1 ring-indigo-50 lg:mr-0 sm:mr-28 xl:mr-16 xl:origin-center" />
+
+				<div className="container py-24 text-center lg:py-32">
+					<h1 className="scroll-m-20 font-extrabold text-4xl tracking-tight lg:text-5xl">
+						Dev Sculpt
+					</h1>
+					<p className="mt-3 text-muted-foreground text-xl">
+						Procure empregos, faça testes e se construa!!
+					</p>
+					<div className="relative mx-auto mt-7 max-w-xl sm:mt-12">
+						<form className="relative z-10 flex space-x-3 rounded-lg border bg-background p-3 shadow-lg">
+							<div className="flex-[1_0_0%]">
+								<Label htmlFor="job" className="sr-only">
+									Procurar vagas
+								</Label>
+								<Input
+									name="job"
+									className="h-full focus-visible:ring-1"
+									id="job"
+									placeholder="Procurar por palavras chaves, Ex: Full Ftack, Designer..."
+								/>
+							</div>
+							<Button size="icon">
+								<span className="sr-only">Pesquisar</span>
+								<SearchIcon />
+							</Button>
+						</form>
+
+						<OrangeStripes />
+						<BlueStripes />
+					</div>
+					<div className="mt-10 flex flex-wrap justify-center gap-2 sm:mt-20">
+						<Button variant="outline">
+							<BriefcaseIcon className="mr-2 h-auto w-3 flex-shrink-0" />
+							Cargos de gerência
+						</Button>
+						<Button variant="outline">
+							<SettingsIcon className="mr-2 h-auto w-3 flex-shrink-0" />
+							Scrum master, QA
+						</Button>
+						<Button variant="outline">
+							<HeartIcon className="mr-2 h-auto w-3 flex-shrink-0" />
+							Desenvolvimento
+						</Button>
+						<Button variant="outline">
+							<LightbulbIcon className="mr-2 h-auto w-3 flex-shrink-0" />
+							Mobile
+						</Button>
+						<Button variant="outline">
+							<FlowerIcon className="mr-2 h-auto w-3 flex-shrink-0" />
+							Desktop
+						</Button>
+						<Button variant="outline">
+							<MountainSnow className="mr-2 h-auto w-3 flex-shrink-0" />
+							Análise de testes
+						</Button>
+					</div>
+					<p className="mt-4 text-sm opacity-50">
+						Tudo isso e muito mais você encontra aqui na <b>Dev Sculpt</b>
+					</p>
+				</div>
+			</main>
+		</>
 	);
 }
---
+++ apps/web/public/assets/orange-stripes.tsx
@@ -0,0 +1,32 @@
+export function OrangeStripes(): JSX.Element {
+	return (
+		<svg
+			className="-translate-y-12 absolute end-0 top-0 hidden h-auto w-16 translate-x-20 text-orange-500 md:block"
+			width={121}
+			height={135}
+			viewBox="0 0 121 135"
+			fill="none"
+			xmlns="http://www.w3.org/2000/svg"
+		>
+			<title>Orange striped</title>
+			<path
+				d="M5 16.4754C11.7688 27.4499 21.2452 57.3224 5 89.0164"
+				stroke="currentColor"
+				strokeWidth={10}
+				strokeLinecap="round"
+			/>
+			<path
+				d="M33.6761 112.104C44.6984 98.1239 74.2618 57.6776 83.4821 5"
+				stroke="currentColor"
+				strokeWidth={10}
+				strokeLinecap="round"
+			/>
+			<path
+				d="M50.5525 130C68.2064 127.495 110.731 117.541 116 78.0874"
+				stroke="currentColor"
+				strokeWidth={10}
+				strokeLinecap="round"
+			/>
+		</svg>
+	);
+}
